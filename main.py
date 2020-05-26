a2f7751cd1578390635226ae472cb685d8238501
gaelmir <50625509+gaelmir@users.noreply.github.com>
2020-05-26 15:40:01 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,26 +1,48 @@
 # -*- coding: utf-8 -*-
 """
-Created on Tue May 26 14:31:40 2020
-
-@author: ASUS
+Created on Fri May 22 10:02:24 2020
+@author: Faustine MICHEL & Gael MIRAMOND
+ICT-UNIT040
+Main Program 
 """
+
+#Import all the usefull libraries 
 import tensorflow as tf
-
 import tensorflow_hub as hub
-
 import matplotlib.pyplot as plt
 import numpy as np
 import os
 import pandas as pd
 import seaborn as sns
-import library_object_recognition_test as lort
 
+#Libraries made for the project 
+import library_for_classification as lfc
+import library_for_pre_post_processing as lfpp
+
+# Global variables for file paths
+path_obj_file=r"obj_file\all_obj.obj"
+path_txt_files=r"txt_files\\"
+path_predicted_labels=r"predictions\predicted_labels.txt"
+path_gameObject_names =r"gameObjects_in_scene\list_of_gameObjects.txt" 
+path_classified_objects=r"predictions\\"
+path_module = r"module\\" 
+path_model = r"Model_tensor"
+
+# Variables for the classifier
 number_of_classes=3
 reject=0.7
-path_test_dataset='demo6'
-path_module = r"C:\Users\ASUS\Desktop\module\\" 
-path_model = r"C:\Users\ASUS\Anaconda3\envs\tensorflow_cpu\programmegael\recognize\Model_tensor"
-path_predicted_labels = "predicted_labels.txt"
+
+# -----------------------------Program begins ----------------------------#                
+#from the .obj file, as many text files are created as the number of GameObject in the unity scene
+lfpp.separate_text(path_obj_file,path_txt_files)
+
+#List all the .txt file in alphabetical order
+lfpp.list_files(path_txt_files,path_gameObject_names)
+
+#Classification of the test set by the classifier
+lfc.object_recognition_classifier(number_of_classes, path_txt_files, path_model, path_module, reject, path_predicted_labels)
+
+#According to the results 
+lfpp.results_processing(path_gameObject_names, path_predicted_labels,path_classified_objects)
 
 
-lort.object_recognition_classifier(number_of_classes, path_test_dataset, path_model, path_module, reject, path_predicted_labels)
